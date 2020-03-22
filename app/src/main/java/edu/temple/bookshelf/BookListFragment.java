289d18e223b1448636350fc78dec0900727eb42e
Shakeel Alibhai <shakeel.alibhai419@gmail.com>
2020-03-22 11:39:43 -0400

    Work on onCreateView() method in BookListFragment

--- app/src/main/java/edu/temple/bookshelf/BookListFragment.java
+++ app/src/main/java/edu/temple/bookshelf/BookListFragment.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -41,7 +42,8 @@ public class BookListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_book_list, container, false);
+        ListView view = (ListView)inflater.inflate(R.layout.fragment_book_list, container, false);
+        return view;
     }
 
     public static BookListFragment newInstance(ArrayList<HashMap> books) {
