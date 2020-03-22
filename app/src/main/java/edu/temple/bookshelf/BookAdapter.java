82f5747755de8cf44f45db134c3cbbec246c1e3d
Shakeel Alibhai <shakeel.alibhai419@gmail.com>
2020-03-22 11:44:50 -0400

    Start working on BookAdapter

---
+++ app/src/main/java/edu/temple/bookshelf/BookAdapter.java
@@ -0,0 +1,40 @@
+package edu.temple.bookshelf;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class BookAdapter extends BaseAdapter {
+
+    private Context c;
+    private ArrayList<HashMap> data;
+
+    public BookAdapter(Context c, ArrayList<HashMap> data) {
+        this.c = c;
+        this.data = data;
+    }
+
+    @Override
+    public int getCount() {
+        return data.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
