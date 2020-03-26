5ea3b2eee3a6d280f835d1b182e3dc05a58168ea
Shakeel Alibhai <shakeel.alibhai419@gmail.com>
2020-03-26 10:37:30 -0400

    Fix bug in MainActivity & add documentation

--- app/src/main/java/edu/temple/bookshelf/MainActivity.java
+++ app/src/main/java/edu/temple/bookshelf/MainActivity.java
@@ -47,43 +47,46 @@ public class MainActivity extends AppCompatActivity implements BookListFragment.
         arrayList.add(book1);
 
         HashMap<String, String> book2 = new HashMap<String, String>();
-        book1.put("Title 2", "Author 2");
+        book2.put("Title 2", "Author 2");
         arrayList.add(book2);
 
         HashMap<String, String> book3 = new HashMap<String, String>();
-        book1.put("Title 3", "Author 3");
+        book3.put("Title 3", "Author 3");
         arrayList.add(book3);
 
         HashMap<String, String> book4 = new HashMap<String, String>();
-        book1.put("Title 4", "Author 4");
+        book4.put("Title 4", "Author 4");
         arrayList.add(book4);
 
         HashMap<String, String> book5 = new HashMap<String, String>();
-        book1.put("Title 5", "Author 5");
+        book5.put("Title 5", "Author 5");
         arrayList.add(book5);
 
         HashMap<String, String> book6 = new HashMap<String, String>();
-        book1.put("Title 6", "Author 6");
+        book6.put("Title 6", "Author 6");
         arrayList.add(book6);
 
         HashMap<String, String> book7 = new HashMap<String, String>();
-        book1.put("Title 7", "Author 7");
+        book7.put("Title 7", "Author 7");
         arrayList.add(book7);
 
         HashMap<String, String> book8 = new HashMap<String, String>();
-        book1.put("Title 8", "Author 8");
+        book8.put("Title 8", "Author 8");
         arrayList.add(book8);
 
         HashMap<String, String> book9 = new HashMap<String, String>();
-        book1.put("Title 9", "Author 9");
+        book9.put("Title 9", "Author 9");
         arrayList.add(book9);
 
         HashMap<String, String> book10 = new HashMap<String, String>();
-        book1.put("Title 10", "Author 10");
+        book10.put("Title 10", "Author 10");
         arrayList.add(book10);
 
         return arrayList;
     }
+
+    //Shakeel Alibhai
+    //CIS 3515
 
     @Override
     public void bookSelected(int index) {
