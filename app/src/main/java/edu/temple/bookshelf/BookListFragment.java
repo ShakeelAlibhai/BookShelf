8f534db002f1ab7d242b00e725e9d232c1b1fa93
Shakeel Alibhai <shakeel.alibhai419@gmail.com>
2020-03-29 12:13:27 -0400

    Fixed bug where the incorrect book would sometimes be displayed when choosing from search results

--- app/src/main/java/edu/temple/bookshelf/BookListFragment.java
+++ app/src/main/java/edu/temple/bookshelf/BookListFragment.java
@@ -66,7 +66,7 @@ public class BookListFragment extends Fragment {
         view.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                parentActivity.bookSelected(position);
+                parentActivity.bookSelected(books.get(position).getID() - 1);
             }
         });
 
